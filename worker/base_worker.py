b6fa32c528579a63aa32680097ff59f3f83143fb
Antonio Recalde <antonio.recalde.russo@gmail.com>
2025-09-28 15:10:41 -0700

    cleanup

--- worker/base_worker.py
+++ worker/base_worker.py
@@ -1,6 +1,15 @@
 class BaseWorker:
-    def __init__(self):
-        pass
+    def __init__(self, name, role, model="gpt-3.5-turbo"):
+        self.name = name
+        self.role = role
+        self.model = model # if a worker is not an agent, this can be None
 
     def execute(self, *inputs):
+        """Execute the worker's main function. To be overridden by subclasses."""
         raise NotImplementedError("This method should be overridden by subclasses.")
+
+    # def send_to(self, other_agent, message):
+    #     # Module 7 lab uses this. But I think we shouldn't. Just adding this here to say that.
+    #     # I think our orchestrator should handle all the communication between workers.
+    #     pass
+    
